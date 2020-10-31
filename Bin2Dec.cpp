199d7449a13215ab18195b7dbb7b10708803ed99
bckd00r <bckd00r@info.atombot.org>
2020-10-31 20:57:46 +0000

    Create Bin2Dec.cpp

---
+++ Bin2Dec.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int convertBinaryToDecimal(long long);
+
+int main()
+{
+	int binary;
+	std:cout << "Please enter binary: ";
+	cin >> binary;
+    cout << "Converted from " << binary << " binary to " << convertBinaryToDecimal(binary) << " decimal";
+	return 0;
+}
+
+int convertBinaryToDecimal(long long n)
+{
+    int decNum = 0, i = 0, remainder;
+    while (n!=0)
+    {
+        remainder = n%10;
+        n /= 10;
+        decNum += remainder*pow(2,i);
+        ++i;
+    }
+    return decNum;
+}
